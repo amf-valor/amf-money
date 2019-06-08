92815f910f6d65fd741c99ba4b7c56c2d19e20ed
amf-valor <alan-1503@hotmail.com>
2019-06-08 12:57:44 -0300

    versão inicial PortalApi

---
+++ AmfValor.AmfMoney.PortalApi/Model/TradingBook.cs
@@ -0,0 +1,9 @@
+namespace AmfValor.AmfMoney.PortalApi.Model
+{
+    public class TradingBook
+    {
+        public string Name { get; set; }
+        public double AmountPerCaptal { get; set; }
+        public int RiskGainRelationship { get; set; }
+    }
+}
